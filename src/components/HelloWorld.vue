dc6ac7624be0f15b28bb6a2ddaff77ae369d2da5
mengtianxiang <shaokexin>
2019-11-25 00:08:08 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="hello-world">
+    <van-tabs v-model="active">
+      <van-tab title="标签 1">
+        <Card :row="cardList"></Card>
+      </van-tab>
+      <van-tab title="标签 2">内容 2</van-tab>
+    </van-tabs>
+    <Footer @on-add="add" @on-buy="buy"></Footer>
+  </div>
+</template>
+
+<script>
+import Card from "./card";
+import Footer from "./footer";
+export default {
+  components: { Card, Footer },
+  data() {
+    return {
+      active: 2,
+      cardList: [
+        {
+          img: "https://img.yzcdn.cn/vant/cat.jpeg",
+          title: "猫",
+          price: "200",
+          num: 0
+        }
+      ]
+    };
+  },
+  methods: {
+    add() {
+      this.dialog('加入购物车')
+    },
+    buy() {
+      this.dialog('立即购买')
+    },
+    dialog(message) {
+      this.$dialog
+        .confirm({
+          title: "确认",
+          message: message
+        })
+        .then(() => {
+          // on close
+        });
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.hello-world {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  /deep/ .van-tabs {
+    flex: 1;
+  }
+}
+</style>
---
+++ src/components/card.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="good-card-section">
+    <div class="card" v-for="(item, i) in row" :key="i">
+      <van-image class="card-img" width="70" height="70" :src="item.img" />
+      <div class="card-flex">
+        <div class="title">{{item.title}}</div>
+        <div class="total">
+          <span class="total-price">￥{{item.price}}</span>
+          <van-stepper class="total-num" v-model="item.num" @change="calcPrice($event, item)"/>
+        </div>
+      </div>
+    <div>总价：{{setValue(item)}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    row: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data() {
+    return {
+    }
+  },
+  computed: {
+    setValue() {
+      return (item) => {
+        return item.price * (item.num || 1)
+      }
+    }
+  },
+  methods: {
+      calcPrice(val, item) {
+        this.value = val*item.price
+      }
+  },
+};
+</script>
+
+<style lang="less">
+.good-card-section {
+  .card {
+    display: flex;
+    padding: 10px;
+    .card-flex {
+      flex: 1;
+      margin-left: 20px;
+      line-height: 33px;
+      .title {
+      }
+      .total {
+        display: flex;
+        .total-price {
+        }
+        .total-num {
+          flex: 1;
+          text-align: center;
+        }
+      }
+    }
+  }
+}
+</style>
